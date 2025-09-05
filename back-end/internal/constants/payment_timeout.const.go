83f913235c07106768d6dd8a897dc7cf2a1d7402
rinookta <rinooktavianridwan@gmail.com>
2025-09-05 06:26:47 +0000

    fix duplicate file

--- back-end/internal/constants/payment_timeout.const.go
+++
@@ -1,5 +0,0 @@
-package constants
-
-const (
-	TypePaymentTimeout = "payment:timeout"
-)
--- back-end/internal/modules/studio/requests/cretae_studio.request.go
+++
@@ -1,7 +0,0 @@
-package requests
-
-type CreateStudioRequest struct {
-	Name         string `json:"name" binding:"required"`
-	SeatCapacity uint   `json:"capacity" binding:"required"`
-	FacilityIDs  []uint `json:"facility_ids"`
-}
